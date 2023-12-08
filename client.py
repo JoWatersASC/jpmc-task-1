1f16a3969ae98f7e8d5791f51a0a87bd04a8dcd1
Joshua Waters <joshua.waters365@gmail.com>
2023-12-08 11:09:16 -0500

    Updated client.py

--- client.py
+++ client.py
@@ -28,6 +28,9 @@ QUERY = "http://localhost:8080/query?id={}"
 # 500 server request
 N = 500
 
+# getDataPoint function should return correct tuple of stock name, bid_price, ask_price and price. Note: price of a stock = average of bid and ask
+# getRatio function should return the ratio of the two stock prices
+# main function should output correct stock info, prices and ratio
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
@@ -35,14 +38,19 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    if price_b == 0:
+        return
+
+    ratio = price_a / price_b
+    print(price_a, price_b, ratio)
+    return ratio
 
 
 # Main
@@ -52,8 +60,10 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
